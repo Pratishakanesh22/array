531f3e5df087b02e0d9005ce451d85c22673e104
pratisha kanesh <91782860+Pratishakanesh22@users.noreply.github.com>
2023-01-14 23:52:29 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[]args){
+      int n = 7;
+      
+      //outer lop
+      for(int i= 1; i<=n; i++){
+        //inner loop
+        for(int j= 1 ; j<=n-i+1; j++){
+
+    System.out.print(j);
+}
+
+System.out.println();
+}
+      
+}
+}
+/////
+////
+///
+//
+//////
+/////
+////
+///
+//
+///////////// 
